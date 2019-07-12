89344ff3635c7e5c9387162d7a2a75833bf95190
Saurav Sanyal <saurav.sanyal@lntinfotech.com>
2019-07-12 15:22:48 -0700

    init()

---
+++ PaySmart/src/main/java/com/lti/paysmart/PaySmartApplication.java
@@ -0,0 +1,13 @@
+package com.lti.paysmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaySmartApplication.class, args);
+	}
+
+}
