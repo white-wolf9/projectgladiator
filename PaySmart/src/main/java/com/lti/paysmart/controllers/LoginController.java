4edd511c4f58a68833f807530e5972346ad10b60
AE205_PC10 <AE205_PC10@Infva07104>
2019-07-15 16:33:18 -0700

    Administrator Login Working.

--- PaySmart/src/main/java/com/lti/paysmart/controllers/LoginController.java
+++ PaySmart/src/main/java/com/lti/paysmart/controllers/LoginController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lti.paysmart.dto.AdminLoginDTO;
 import com.lti.paysmart.dto.UserLoginDTO;
+import com.lti.paysmart.interfaces.AdminService;
 import com.lti.paysmart.interfaces.UserService;
 
 @RestController
@@ -18,8 +20,16 @@ public class LoginController {
 	@Autowired
 	UserService userServ;
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@Autowired
+	AdminService admServ;
+	
+	@RequestMapping(value = "/login.user", method = RequestMethod.POST)
 	public String login(@RequestBody UserLoginDTO userLoginDTO) {
 		return userServ.performLogin(userLoginDTO);
+	}
+	
+	@RequestMapping(value = "/login.admin", method = RequestMethod.POST)
+	public String login(@RequestBody AdminLoginDTO adminLoginDTO) {
+		return admServ.performLogin(adminLoginDTO);
 	}
 }
